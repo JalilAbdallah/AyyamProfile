ee9ba77df0149a7be2594058efabbd2acbee0ef0
Abdallah Jalil <019jalil@gmail.com>
2025-05-24 18:52:43 +0300

    Add ProfileInfo component with its subcomponent

--- screens/ProfileScreen/index.js
+++ screens/ProfileScreen/index.js
@@ -8,9 +8,14 @@ import {
 import { styles } from './styles';
 import { USERS } from '../../constants';
 import ProfileHeader from '../../components/profile/ProfileHeader';
+import ProfileInfo from '../../components/profile/ProfileInfo';
 
 const ProfileScreen = ({ userId = 'user_001' }) => {
   const user = USERS.find(u => u.id === userId);
+  
+  const handleProfilePicturePress = () => {
+    // this is for the animation part when the profile picture is pressed
+  };
   
   if (!user) {
     return (
@@ -22,14 +27,17 @@ const ProfileScreen = ({ userId = 'user_001' }) => {
 
   return (
     <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor="#000000" />
       
       <ProfileHeader user={user} />
       
+      <ProfileInfo 
+        user={user} 
+        onProfilePicturePress={handleProfilePicturePress}
+      />
 
-      {/* For testing only - will be replaced */}
       <View style={styles.content}>
-        <Text style={styles.testText}>Profile Screen Working!</Text>
-        <Text style={styles.testText}>User: {user.name}</Text>
+        <Text style={styles.testText}>Content tabs and posts will go here</Text>
       </View>
     </SafeAreaView>
   );
